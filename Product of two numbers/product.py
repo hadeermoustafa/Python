af8678296cd9de5bf720e43773622e14ee20cd2f
Hadeer Moustafa <hadeer.moustafa@outlook.com>
2024-11-06 15:14:30 +0200

    product of two numbers

---
+++ Product of two numbers/product.py
@@ -0,0 +1,8 @@
+# Products of two intgers
+
+num_1 = int(input('Please enter the first number\n\t'))
+num_2 = int(input('Please enter the second number\n\t'))
+
+product = num_1 * num_2
+
+print('The product of ', num_1, ' and ', num_2, ' is ', product)
