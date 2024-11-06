f2111b686ed342e937c5177181d773a59f17cec1
Hadeer Moustafa <hadeer.moustafa@outlook.com>
2024-11-06 15:08:44 +0200

    sum of two ints

---
+++ sum of two integers/sum.py
@@ -0,0 +1,8 @@
+# The sum of two intgers
+
+int_1 = int(input('Please enter the first number\n\t'))
+int_2 = int(input('Please enter the second number\n\t'))
+
+sum = int_1 + int_2
+
+print('The sum of ' + str(int_1) + ' and ' + str(int_2) + ' is ' + str(sum))
