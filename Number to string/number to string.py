0b7acecf1389f4d1ec28008cd4413d193df06fcb
Hadeer Moustafa <hadeer.moustafa@outlook.com>
2024-11-07 14:06:23 +0200

    add new programs

---
+++ Number to string/number to string.py
@@ -0,0 +1,14 @@
+num = int(input('Please enter between 1 and 5 inclusive.\n'))
+
+if num == 1:
+    print('One')
+elif num == 2:
+    print ('Two')
+elif num == 3:
+    print ('Three')
+elif num == 4:
+    print('four')
+elif num == 5:
+    print('Five')
+else:
+    print("Please enter a number within range")
